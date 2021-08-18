c9365b926c37e08f009862cd89fd9f02f677fb7f
StephenDanielArt <dh_8rbpku@william-floyd.dreamhost.com>
2021-08-17 23:15:50 -0700

    fixed css file versioning

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_MOTL_ASTRA_CHILD_VERSION', '1.0.0' );
+define( 'CHILD_THEME_MOTL_ASTRA_CHILD_VERSION', wp_get_theme()->get('Version') );
 
 /**
  * Enqueue styles
