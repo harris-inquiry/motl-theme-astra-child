1e2c714b82065a8131b427a8ca616549b587f075
Harrison Quarry <harrisonqleddy@gmail.com>
2021-09-25 13:55:36 -0700

    Added archive page

---
+++ archive-motl_product.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+	<div id="primary" <?php astra_primary_class(); ?>>
+
+		<?php astra_primary_content_top(); ?>
+
+		<?php astra_archive_header(); ?>
+
+		<!-- ?php astra_content_loop(); ? -->
+
+		<?php if (have_posts()):
+			while (have_posts()): the_post(); ?>
+
+			<?php get_template_part( 'template-parts/motl_product/archive_info' ); ?>
+
+			<?php endwhile; else: ?>
+				<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+		<?php endif; ?>
+		
+
+		<?php astra_pagination(); ?>
+
+		<?php astra_primary_content_bottom(); ?>
+
+	</div><!-- #primary - archive -->
+
+<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+<?php get_footer(); ?>
---
+++ template-parts/motl_product/archive_info.php
@@ -0,0 +1,2 @@
+
+<h2>Archive Post information</h2>
