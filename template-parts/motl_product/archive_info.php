e904806e4d544cdf46e17fc4f98e40078ef3e46d
Harrison Quarry <harrisonqleddy@gmail.com>
2021-11-20 10:15:01 -0800

    Motl images transform on hover

--- template-parts/motl_product/archive_info.php
+++ template-parts/motl_product/archive_info.php
@@ -1,27 +1,27 @@
-
 <article class="pb-6">
-  <div class="relative">
+  <div class="motl-sample relative overflow-hidden border-4">
     <a href="<?php the_permalink(); ?>" rel="bookmark">
       <div style="justify-content:center;">
-      <?php
+        <?php
         $motl_image_id = get_field('motl-image');
         if ($motl_image_id) {
           echo wp_get_attachment_image(
             $motl_image_id,
             "full",
             false,
-            array("class" => "",
-                  "style" => "position:relative;")
+            array(
+              "class" => "motl-sample_image",
+              "style" => "position:relative;"
+            )
           );
         } ?>
       </div>
-    <div class="title-info absolute bottom-0 px-4 py-2 text-white" style="width: 100%;background: linear-gradient(45deg, #000000a3, transparent 60%);">
-      <h2 class="entry-title mb-0 text-white">
-        <?php the_title(); ?>
-      </h2>
-      <p class="italic mb-1"><?php the_field("motl-medium"); ?></p>
-    </div>
+      <div class="motl-sample_text title-info absolute bottom-0 px-4 py-2 text-white">
+        <h2 class="entry-title mb-0 text-white">
+          <?php the_title(); ?>
+        </h2>
+        <p class="italic mb-1"><?php the_field("motl-medium"); ?></p>
+      </div>
     </a>
   </div>
 </article>
-
