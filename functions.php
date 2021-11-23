33efda7d485f1263babaa681389474f8f5959215
Harrison Quarry <harrisonqleddy@gmail.com>
2021-11-23 14:39:19 -0800

    Organizing files

--- functions.php
+++ functions.php
@@ -16,20 +16,36 @@ define( 'CHILD_THEME_MOTL_ASTRA_CHILD_VERSION', wp_get_theme()->get('Version') )
 /**
  * Enqueue styles
  */
-function child_enqueue_styles() {
+function motl_child_enqueue_styles_scripts() {
 	wp_enqueue_style( 'motl-astra-child-theme-css',
                       get_stylesheet_directory_uri() . '/style.css',
                       array('astra-theme-css'),  // parent theme key
                       CHILD_THEME_MOTL_ASTRA_CHILD_VERSION,
                       'all' );
 
-    wp_enqueue_style( 'motl-product-style',
-                      get_stylesheet_directory_uri() . "/assets/css/style-motl_product.css",
-                      array(),
-                      CHILD_THEME_MOTL_ASTRA_CHILD_VERSION );
-    wp_enqueue_style( 'motl-product-tailwind-style',
-                      get_stylesheet_directory_uri() . "/assets/css/tailwind-css.css",
-                      array(),
-                      CHILD_THEME_MOTL_ASTRA_CHILD_VERSION );
+    $current_post = get_post();
+
+    if( motl_is_post_type_motlProduct($current_post) ) {
+        wp_enqueue_style( 'motl-product-style',
+                        get_stylesheet_directory_uri() . "/build/css/style-motl_product.css",
+                        array(),
+                        CHILD_THEME_MOTL_ASTRA_CHILD_VERSION );
+        wp_enqueue_style( 'motl-product-tailwind-style',
+                        get_stylesheet_directory_uri() . "/build/css/tailwind-css.css",
+                        array(),
+                        CHILD_THEME_MOTL_ASTRA_CHILD_VERSION );
+
+        /*wp_enqueue_script( 'motl-javascript',
+                        get_stylesheet_directory_uri() . "/build/js/product-page.js",
+                        array(),
+                        CHILD_THEME_MOTL_ASTRA_CHILD_VERSION );*/
+    }
 }
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action( 'wp_enqueue_scripts', 'motl_child_enqueue_styles_scripts', 15 );
+
+function motl_is_post_type_motlProduct( $current_post ) {
+    if( is_null($current_post) ) {
+        return FALSE;
+    }
+    return 'motl_product' == $current_post->post_type;
+}
---
+++ src/php/motl_product/cover.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Product cover for MOTL art pages
+ * @since 1.0.0
+ */
+
+function conditional_get_table_row($label, $field_name)
+{
+  if (get_field($field_name)) { ?>
+    <tr>
+      <td><?php echo $label; ?></td>
+      <td><?php the_field($field_name); ?></td>
+    </tr>
+<?php
+  }
+}
+?>
+
+<div class="container mx-auto p-4">
+
+  <div id="preview-content">
+    <div id="title" class="sticky top-0 flex justify-between z-50 bg-white py-2 opacity-95 border-2 rounded">
+      <div class="ml-4 my-auto">
+        <h1 class="font-medium mb-1 text-4xl"><?php echo the_title(); ?></h1>
+        <p class="text-gray-400 mb-1"><?php the_field("motl-medium"); ?></p>
+      </div>
+    </div>
+
+    <div class="sm:flex my-2 sm:space-x-2">
+
+      <div id="left-content" class="flex-shrink-0 sm:max-w-1/2">
+        <div class="sticky top-24">
+
+          <?php
+          $motl_image_id = get_field('motl-image');
+          if ($motl_image_id) {
+            echo wp_get_attachment_image(
+              $motl_image_id,
+              "full",
+              false,
+              array(
+                "class" => "p-2 mx-auto",
+                "id" => "artwork-image"
+              )
+            );
+          } ?>
+        </div>
+      </div>
+
+      <div id="right-content" class="mt-4">
+        <div class="mb-8 mt-4">
+          <?php $motl_quote_var = "motl-tag-line";
+          if (get_field($motl_quote_var)) : ?>
+            <blockquote class="mb-6 font-semibold text-2xl text-gray-600">
+              <?php the_field($motl_quote_var); ?>
+            </blockquote>
+          <?php endif; ?>
+
+          <div id="description">
+            <?php if (get_field("motl-description")) {
+              the_field("motl-description");
+            } ?>
+          </div>
+        </div>
+
+        <div id="contacts" class="my-4 mb-8">
+          <div class="p-2 flex space-x-2">
+            <a href="/contact">
+              <button class="text-white bg-gray-800 font-bold px-6 py-3">Inquire</button>
+            </a>
+          </div>
+        </div>
+
+        <table>
+          <tr>
+            <td>Title</td>
+            <td><?php echo the_title(); ?></td>
+          </tr>
+          <?php conditional_get_table_row("Medium", "motl-medium"); ?>
+          <?php conditional_get_table_row("Dimensions", "motl-dimensions"); ?>
+          <?php conditional_get_table_row("Date Created", "motl-date-created"); ?>
+          <?php conditional_get_table_row("Subject", "motl-subject"); ?>
+          <?php conditional_get_table_row("Inspiration", "motl-inspiration"); ?>
+          <?php conditional_get_table_row("Theme", "motl-theme"); ?>
+        </table>
+      </div>
+    </div>
+  </div>
+
+  <hr class="my-8">
+</div>
